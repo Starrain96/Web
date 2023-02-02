90405166e5e7c68d10da43971d93313f0c34c870
Starrain96 <124110590+Starrain96@users.noreply.github.com>
2023-02-02 17:38:27 +0900

    Add files via upload

---
+++ day04/out.js
@@ -0,0 +1,18 @@
+//함수는 여러 절차가 있는 처리를 이름을 부여하여 기능을 정의한다.
+//function
+function 빨래하다() {
+    console.log("1. 먼지를 털다.")
+    console.log("2. 물에 집어 넣는다.")
+    console.log("3. 비누를 바른다.")
+    console.log("4. 오물조물 비빈다.")
+    console.log("5. 물에 여러번 헹군다.")
+    console.log("6. 짠다.")
+    console.log("7. 넌다.")
+}
+
+//청소하다에 대한 함수정의해서 사용해보세요.
+
+function 청소하다() {
+    console.log("청소를 할까..?")
+    console.log("청소를 말까..?")
+}
---
+++ day04/out2.js
@@ -0,0 +1,13 @@
+function 여행하다() {
+    console.log("여행 출발!")
+}
+function 쉬다() {
+    console.log("쉬는 시간!")
+
+}
+
+function 세번() {
+    for (let hi = 0; hi < 3; hi++) {
+        alert("@@@") 
+    }
+}
